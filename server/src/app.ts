5ad388633583ba5296a2e61c03c05057e0a38435
kfiggins1 <kfiggins@spotinc.com>
2020-05-20 00:06:51 -0400

    added a simple start for graphql

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,30 @@
 //To get started: https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
 import express from 'express';
+import graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 const app = express();
 const port = 3000;
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
