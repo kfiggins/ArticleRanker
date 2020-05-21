0cf62061248fabe7230d64a8eff2d9cbbcf3a0d7
kfiggins1 <kfiggins@spotinc.com>
2020-05-20 23:43:15 -0400

    Added client side

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {count}
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
