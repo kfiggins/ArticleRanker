0f629c06efc7ab5546bd375db93634ccc27b6582
kfiggins1 <kfiggins@spotinc.com>
2020-05-29 00:52:25 -0400

    made some good progress with graphql and sequelize. ripped out typescript.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,5 @@
-//To get started: https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
-import express from "express";
-import graphqlHTTP from "express-graphql";
-import { buildSchema } from "graphql";
-import { ApolloServer, gql } from "apollo-server";
+const express = require("express")
+const { ApolloServer, gql } = require("apollo-server-express");
 const db = require("./models/index.js");
 
 const typeDefs = gql`
@@ -14,11 +11,17 @@ const typeDefs = gql`
     author: String
   }
 
+  type Platform{
+    id: Int
+    name: String
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
+    platform: [Platform]
   }
 `;
 
@@ -37,9 +40,12 @@ const books = [
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: (root: any, args: any, { db }: any) => {
-      console.log(root, args, db);
+    books: (root, args, { db }) => {
       return books;
+    },
+    platform: (root, args, { db }) => {
+      console.log(db.Platform)
+      return db.Platform.findAll({ attributes: ["name"] });
     },
   },
 };
@@ -48,12 +54,11 @@ const resolvers = {
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers, context: { db } });
 
-// The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
-
 const app = express();
+
+
+server.applyMiddleware({ app }); // app is from an existing express app. Mount Apollo middleware here. If no path is specified, it defaults to `/graphql`.
+db.sequelize.sync()
 
 const port = 3000;
 
